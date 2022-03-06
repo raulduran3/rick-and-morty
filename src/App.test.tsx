5ccafbe3d25d2cf46c7ce388f443e7f183e4be02
Raul Duran <rauladuran@hotmail.com>
2022-03-06 13:25:15 +0100

    Remove test

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders Rick & Morty App", () => {
-  render(<App />);
-  expect(screen.getByTestId("test-id")).toBe("Rick & Morty App");
-});
