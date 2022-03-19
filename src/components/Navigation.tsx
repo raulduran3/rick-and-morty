bf71c00e9f99e936b7d1b55d6323d618b1b71d1e
Raul Duran <rauladuran@hotmail.com>
2022-03-19 03:55:26 +0100

    Add app title to Navigation Component

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -5,28 +5,36 @@ import { Link } from "react-router-dom";
 
 const NavigationComp = () => {
   return (
-    <Navbar bg="dark" variant="dark">
-      <Container>
-        <Link style={{ textDecoration: "none", color: "white" }} to="/">
-          Home
-        </Link>
-        <Link
-          style={{ textDecoration: "none", color: "white" }}
-          to="/characters"
-        >
-          Characters
-        </Link>
-        <Link style={{ textDecoration: "none", color: "white" }} to="/episodes">
-          Episodes
-        </Link>
-        <Link
-          style={{ textDecoration: "none", color: "white" }}
-          to="/locations"
-        >
-          Locations
-        </Link>
-      </Container>
-    </Navbar>
+    <>
+      <header>
+        <h1>Rick & Morty App</h1>
+      </header>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Link style={{ textDecoration: "none", color: "white" }} to="/">
+            Home
+          </Link>
+          <Link
+            style={{ textDecoration: "none", color: "white" }}
+            to="/characters"
+          >
+            Characters
+          </Link>
+          <Link
+            style={{ textDecoration: "none", color: "white" }}
+            to="/episodes"
+          >
+            Episodes
+          </Link>
+          <Link
+            style={{ textDecoration: "none", color: "white" }}
+            to="/locations"
+          >
+            Locations
+          </Link>
+        </Container>
+      </Navbar>
+    </>
   );
 };
 
