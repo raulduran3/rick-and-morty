578a744345f86aaf5786ca91227b4c8e7b7dcb22
Raul Duran <rauladuran@hotmail.com>
2022-03-19 03:55:05 +0100

    Remove Header Component

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import Spinner from "react-bootstrap/Spinner";
-import Header from "./Header";
 
 const LoadingComp = () => {
   return (
     <>
-      <Header />
       <Spinner className="spinner" animation="border" role="status">
         <span className="visually-hidden">Loading...</span>
       </Spinner>
