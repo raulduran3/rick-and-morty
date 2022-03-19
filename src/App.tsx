c09446d105bc7066adfc0e1c523bb012f40174b8
Raul Duran <rauladuran@hotmail.com>
2022-03-19 03:55:54 +0100

    Use Navigation Component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import NavigationComp from "./components/Navigation";
 import HomeView from "./views/Home/HomeView";
 import CharactersView from "./views/Characters/CharactersView";
 import CharacterView from "./views/Characters/CharacterView";
@@ -18,17 +20,20 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomeView />} />
-          <Route path="/characters" element={<CharactersView />} />
-          <Route path="/character" element={<CharacterView />} />
-          <Route path="/episodes" element={<EpisodesView />} />
-          <Route path="/episode" element={<EpisodeView />} />
-          <Route path="/locations" element={<LocationsView />} />
-          <Route path="/location" element={<LocationView />} />
-        </Routes>
-      </BrowserRouter>
+      <Container fluid className="container">
+        <BrowserRouter>
+          <NavigationComp />
+          <Routes>
+            <Route path="/" element={<HomeView />} />
+            <Route path="/characters" element={<CharactersView />} />
+            <Route path="/character" element={<CharacterView />} />
+            <Route path="/episodes" element={<EpisodesView />} />
+            <Route path="/episode" element={<EpisodeView />} />
+            <Route path="/locations" element={<LocationsView />} />
+            <Route path="/location" element={<LocationView />} />
+          </Routes>
+        </BrowserRouter>
+      </Container>
     </ApolloProvider>
   );
 }
